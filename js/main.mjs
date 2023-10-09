ec56c4d4abe8d6074fcf1e260212ef7da95486d4
nazarpunk <nazarpunk@gmail.com>
2023-10-09 20:19:53 +0300

    angle normalization

--- js/main.mjs
+++ js/main.mjs
@@ -1,1 +1,5 @@
-document.querySelectorAll('a').forEach(a => a.target = '_blank')
+document.querySelectorAll('a').forEach(a => {
+    a.target = '_blank';
+    const href = a.getAttribute('href');
+    if (href.startsWith('#')) a.setAttribute('href', `/wc3-geometry${href}`)
+})
