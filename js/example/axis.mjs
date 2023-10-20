e25ad1e589602c06f70150f59dbedfb115366b67
nazarpunk <nazarpunk@gmail.com>
2023-10-20 03:04:46 +0300

    new Grid (touch)

--- js/example/axis.mjs
+++ js/example/axis.mjs
@@ -1,29 +1,14 @@
-import {Canvas} from '../draw/canvas.mjs'
-import {Axis} from '../draw/axis.mjs'
 import {Point} from '../math/point.mjs'
 import {Grid} from '../draw/grid.mjs'
 
-const axis = new Axis()
-
 const A = new Point(2, 5)
 
-if (0) {
-    Canvas.observe(document.querySelector('.canvas-axis'), c => {
+const grid = new Grid(document.querySelector('.canvas-axis'), () => {
+    grid
+        .grid(.5, .5)
+        .point(A, {track: true})
 
-        axis.draw(c, {
-            centerX: c.width * .5,
-            centerY: c.height * .5
-        }).point(A.move(axis.mouseX, axis.mouseY), {track: true})
+    grid.pointMove(A)
 
-    })
-} else {
-    const grid = new Grid(document.querySelector('.canvas-axis'), () => {
-        grid
-            .grid(.5, .5)
-            .point(A, {track: true})
-
-        grid.pointMove(A)
-
-    })
-    Grid.observe(grid)
-}
+})
+Grid.observe(grid)
